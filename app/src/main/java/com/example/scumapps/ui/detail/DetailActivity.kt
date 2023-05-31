3706f8c8a2fdc615f33d732555a2ed2da04e0bd2
CharleneditaR <criyanne@gmail.com>
2023-05-31 18:52:06 +0700

    updated

--- app/src/main/java/com/example/scumapps/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/scumapps/ui/detail/DetailActivity.kt
@@ -3,12 +3,11 @@ package com.example.scumapps.ui.detail
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
-import com.example.scumapps.ui.HomeActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.scumapps.R
 import com.example.scumapps.databinding.ActivityDetailBinding
+import com.example.scumapps.ui.HomeActivity
 
 class DetailActivity : AppCompatActivity() {
 
@@ -24,6 +23,13 @@ class DetailActivity : AppCompatActivity() {
         when (intent.getBooleanExtra("type", true)) {
             true -> binding.objectPhoto.setImageBitmap(IMAGEBITMAP)
             false -> binding.objectPhoto.setImageURI(IMAGEURI)
+        }
+
+        when(binding.wasteType.text) {
+            "PET" -> binding.wasteDesc.text = getString(R.string.PET)
+            "Glass" -> binding.wasteDesc.text = getString(R.string.Glass)
+            "AluCan" -> binding.wasteDesc.text = getString(R.string.AluCan)
+            else -> binding.wasteDesc.text = "No description for this image"
         }
 
         binding.backHomeBtn.setOnClickListener {
